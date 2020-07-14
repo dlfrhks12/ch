8c9a7a6258a3b46d6a58c84851f981d4e8a24ea2
8 <9@DESKTOP-IV5GNBN>
2020-07-14 16:19:42 +0900

    수밍!

---
+++ cheatingday/src/main/java/com/icia/cheatingday/cart/KakaoPayReadyVO.java
@@ -0,0 +1,11 @@
+package com.icia.cheatingday.cart;
+
+import java.util.Date;
+
+import lombok.Data;
+
+@Data
+public class KakaoPayReadyVO {
+	private String tid,next_redirect_pc_url;
+	private Date created_at;
+}

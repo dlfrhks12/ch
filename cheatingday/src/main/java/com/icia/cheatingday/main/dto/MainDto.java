de26e86987a7df948e64060476564346b485a909
13 <13@DESKTOP-T87MBHP>
2020-07-14 15:56:52 +0900

    윤용주 커밋

---
+++ cheatingday/src/main/java/com/icia/cheatingday/main/dto/MainDto.java
@@ -0,0 +1,23 @@
+package com.icia.cheatingday.main.dto;
+
+import lombok.*;
+import lombok.experimental.*;
+
+public class MainDto {
+	private MainDto() {}
+	@Data
+	@AllArgsConstructor
+	@NoArgsConstructor
+	@Builder
+	@Accessors(chain=true)
+	public static class DtoForList{
+		private int sNum;
+		private String sName;
+		private String sSajin;
+		private int foodNo;
+		private String foodCategory;
+		private Integer sReviewCnt;
+		private Integer sStarPoint;
+	}
+}
+
